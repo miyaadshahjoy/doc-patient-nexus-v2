50f0215456f7d4b0c7c91b3a401ccc3263780b00
Miyaad Shah Joy <miyaadshahjoy@gmail.com>
2025-05-31 12:09:02 +0600

    Geospatial queries added

--- generateDocs.js
+++ generateDocs.js
@@ -1,6 +1,5 @@
-// generateDocs.js
 const fs = require('fs');
-const swaggerSpec = require('./docs/config/swaggerConfig');
+const swaggerSpec = require('./docs/config/swagger-config');
 
 fs.writeFileSync('./openapi.json', JSON.stringify(swaggerSpec, null, 2));
 console.log('✅ Swagger spec written to openapi.json');
